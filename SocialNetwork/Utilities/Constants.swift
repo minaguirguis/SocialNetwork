50c547e7a038d822e340d8eaf9a9539d9f822687
Mina Guirguis <MGuirguis.10@gmail.com>
2018-03-31 19:21:32 -0400

    Added Custom classes to sign in screen

---
+++ SocialNetwork/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SocialNetwork
+//
+//  Created by Mina Guirguis on 3/31/18.
+//  Copyright © 2018 Mina Guirguis. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
