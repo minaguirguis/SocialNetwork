01e9b364ff305585c6b15d3b99aff4f6c52be7fc
Mina Guirguis <MGuirguis.10@gmail.com>
2018-04-15 00:07:12 -0400

    Updated

--- SocialNetwork/Model/RoundButton.swift
+++ SocialNetwork/Model/RoundButton.swift
@@ -15,7 +15,7 @@ class RoundButton: UIButton {
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
-        layer.shadowRadius = 5.0
+        layer.shadowRadius = 6.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         imageView?.contentMode = .scaleAspectFit
     }
