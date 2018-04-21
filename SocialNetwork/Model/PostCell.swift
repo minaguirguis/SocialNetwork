9bc8fa06624b2b734864d73cee2af9bce9bd14e1
Mina Guirguis <MGuirguis.10@gmail.com>
2018-04-20 23:40:34 -0400

    Added Custom Cells to tableView

---
+++ SocialNetwork/Model/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  SocialNetwork
+//
+//  Created by Mina Guirguis on 4/20/18.
+//  Copyright © 2018 Mina Guirguis. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+   
+    
+
+}
