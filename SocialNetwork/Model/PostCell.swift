631ba906603bded3c9fe2c4f9c41b982facc2969
Mina Guirguis <MGuirguis.10@gmail.com>
2018-05-10 14:32:26 -0400

    Downloaded images from Firebase Storage and cached them

--- SocialNetwork/Model/PostCell.swift
+++ SocialNetwork/Model/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     @IBOutlet weak var profileImg: UIImageView!
@@ -22,11 +23,34 @@ class PostCell: UITableViewCell {
         
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) { //at this point the image is in the cache not a URL
         
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        if img != nil { //if the cache has an image then set it, if not then download it
+            self.postImg.image = img
+        } else {
+            let ref = Storage.storage().reference(forURL: post.imageUrl)
+            
+            ref.getData(maxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("MINA: Unable to download image from Firebase storage")
+                } else {
+                    print("MINA: Image downloaded from Firebase storage")
+                    if let imageData = data {
+                        if let img = UIImage(data: imageData){
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                            print("MINA: Image Set in Cache")
+                        }
+                    }
+                }
+                
+            })
+        }
+        
     }
     
 
--- SocialNetwork/Services/DataService.swift
+++ SocialNetwork/Services/DataService.swift
@@ -10,14 +10,20 @@ import Foundation
 import Firebase
 
 let DB_BASE = Database.database().reference()//this will contain the URL of the root of our databse
+let STORAGE_BASE = Storage.storage().reference()//Accessing the storage from Firebase with this
 
 class DataService {
     
     static let ds = DataService()//created singleton
     
+    //DB references
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child(POSTS_REF)//getting the object posts
     private var _REF_USERS = DB_BASE.child(USERS_REF)
+    
+    //Storage references
+    private var _REF_POST_IMAGES = STORAGE_BASE.child(POST_STORAGE_REF)
+    //file the same way for profile images
     
     var REF_BASE: DatabaseReference {
         return _REF_BASE
@@ -31,12 +37,16 @@ class DataService {
         return _REF_USERS
     }
     
+    var REF_POST_IMAGES: StorageReference {
+        return _REF_POST_IMAGES
+    }
+    
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         REF_USERS.child(uid).updateChildValues(userData)
         //its referencing users and creating a uid if one is not found
         print("MINA: Successfully created user in Firebase")
     }
-    
+
     
     
 }
